189791ba10874fbe5ee509bf84c6cf56d0929554
FaircoreHD <nafis23harwin2001@gmail.com>
2021-04-26 12:40:22 +0700

    reinit next-project

---
+++ pages/login.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Jumbotron from '../components/jumbotron'
+import Article from '../components/article'
+import Pagination from '../components/pagination'
+import Sidebar from '../components/sidebar'
+import Layout from '../components/layout'
+
+export default function Login() {
+  return (
+    <div>
+      <Layout>
+        <div className="row">
+          <div className="col-md-8">
+            <h3 className="pb-4 mb-4 fst-italic border-bottom">
+              From the Firehose
+            </h3>
+          </div>
+          <Sidebar/>
+        </div>
+      </Layout>
+    </div>
+  )
+}
